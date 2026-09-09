6997172cdd4952338d4676f8a0fa0dbf802afc5d
Lovekesh Arya <lovekesh.arya@nagarro.com>
2026-09-09 16:46:19 +0530

    added env

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  appName: 'InkFlow',
+  useFirebaseAuth: true,
+  firebase: {
+    apiKey: 'AIzaSyCGUk9V135XR2ge-WgBKNVob4unMH-7o1E',
+    authDomain: 'inkflow-14ca1.firebaseapp.com',
+    projectId: 'inkflow-14ca1',
+    storageBucket: 'inkflow-14ca1.firebasestorage.app',
+    messagingSenderId: '515365615595',
+    appId: '1:515365615595:web:c8dc2b8f93bb53748c8bae'
+  }
+};
